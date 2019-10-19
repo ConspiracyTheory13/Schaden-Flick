8c23d55089f1ac90d947c3ee87ab8571c4b85091
Charla Bigelow <33845593+ConspiracyTheory13@users.noreply.github.com>
2019-10-19 12:51:45 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,43 +2,23 @@
 const OMDbAPIKey="&apikey=5cb6d69e"
 const MDBKey= "&api_key=b634ceab64cc409d417a98a2cfa729ba";
 
-
-//bc the discover feature is special you'll have to do an 
-//if else with ||'s
-
-// an api key needs to have the beginning URL, the limit,
-//
 $(".pickYourPoison").submit(function(event){
     event.preventDefault();  
     let url= 'https://api.themoviedb.org';
     let genre="/3/discover/movie?with_genres="
-    // let audioType = $("option:selected")
     let keywords="&with_keywords=" + $(".queries").val();
-    let page="&page=" 
+    let page="&page="
+    let popularity ="&sort_by=vote_average.asc"
+    if ($('input[name=answer]:checked', '.pickYourPoison').val()==='no')
+    {
+    popularity = "&sort_by=vote_average.dec";
+    }     
+    console.log($('input[name=answer]:checked', '.pickYourPoison').val());
+    let selectedLanguage= "&language=" + $(".chooseLanguage option:selected").val(); 
     let userInput = $("option:selected");
-    console.log(userInput);
-    // first figure out your for loop. finish editing it
-    //one more option, language?
-    //use posters
-    //get link working next
-
-    // let discover= '/discover/movie?'
-    // let limitSet ='&limit='+$(".numberOfResults").val();
-    // let onlyEnglish ="&language=en-US"
-    // let subbedOnly = ""
-    // empty();
-    //with_genres
-    // console.log("userinput" + userInput)
-      genre+=`${userInput.attr("id")}`;
-    //   console.log('for loop firing')
-    //   console.log(userInput[i]);
-    //   console.log(defaultValue)
-    //   console.log(genre)
-    //   if(i < userInput.length-1) {
-    //     states+=',';
-    //   }
+    genre+=`${userInput.attr("id")}`;
     console.log("being fetched")
-    url+=genre+keywords+MDBKey
+    url+=genre+MDBKey+selectedLanguage+popularity
     console.log(url)
 
     fetch(url)
@@ -46,37 +26,10 @@ $(".pickYourPoison").submit(function(event){
     .then(responseJson => printResults(responseJson));
    });
 
-function getPages(results){
-
-}
-
 function printResults(resultObject) {
-  console.log("runningPrintResults")
-  console.log(resultObject);
-  let data = resultObject.results;
+    $("#resultsContainer").empty();
+    let data = resultObject.results;
     for(let i=0; i < data.length; i++) {
-      $('.mainContainer').append(data[i].title + '<br>');
+      $('#resultsContainer').append(data[i].title + '<br>');
     };
     }
-
-
-    //`<li>${data[i].title} + ${data[i].vote_average} + ${data[i].overview}</li>`
-     
-    function empty(){
-        console.log("empty")
-        $(".mainContainer").empty();
-      };
-    
-
-
-//api.themoviedb.org/4/list/10?page=1&api_key=b634ceab64cc409d417a98a2cfa729ba&sort_by=vote_average.desc
-
-//make the "vampires" randomly generate
-//comparison through tags
-
-//https://api.themoviedb.org/3/genre/movie/list?api_key=&api_key=b634ceab64cc409d417a98a2cfa729ba&language=en-US
-//list of genres
-//popularity
-//ratings
-//I like the idea of searching certain actors, but I don't know a good way to do it without it wasting time
-//how about an awful dinner to go with it?
