f610dd8c5d263748807bb30d0b2ac7f88656a2d6
Charla Bigelow <33845593+ConspiracyTheory13@users.noreply.github.com>
2019-10-19 12:52:43 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
+const MDBKey= "&api_key=b634ceab64cc409d417a98a2cfa729ba";
 
-const OMDbAPIKey="&apikey=5cb6d69e"
+$(".pickYourPoison").submit(function(event){
+    event.preventDefault();  
+    let url= 'https://api.themoviedb.org';
+    let genre="/3/discover/movie?with_genres="
+    let keywords="&with_keywords=" + $(".queries").val();
+    let page="&page="
+    let popularity ="&sort_by=vote_average.asc"
+    if ($('input[name=answer]:checked', '.pickYourPoison').val()==='no')
+    {
+    popularity = "&sort_by=vote_average.dec";
+    }     
+    console.log($('input[name=answer]:checked', '.pickYourPoison').val());
+    let selectedLanguage= "&language=" + $(".chooseLanguage option:selected").val(); 
+    let userInput = $("option:selected");
+    genre+=`${userInput.attr("id")}`;
+    console.log("being fetched")
+    url+=genre+MDBKey+selectedLanguage+popularity
+    console.log(url)
+
+    fetch(url)
+    .then(response => response.json())
+    .then(responseJson => printResults(responseJson));
+   });
+
+function printResults(resultObject) {
+    $("#resultsContainer").empty();
+    let data = resultObject.results;
+    for(let i=0; i < data.length; i++) {
+      $('#resultsContainer').append(data[i].title + '<br>');
+    };
+    }
